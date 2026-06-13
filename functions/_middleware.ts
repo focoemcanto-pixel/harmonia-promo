16fc06deddbfb5c255b2dd7d669a479dde8a4afa
focoemcanto-pixel <focoemcanto@gmail.com>
2026-06-12 22:03:21 -0300

    Route oferta domain to AB test page

---
+++ functions/_middleware.ts
@@ -0,0 +1,17 @@
+export const onRequest: PagesFunction = async (context) => {
+  const request = context.request
+  const url = new URL(request.url)
+  const host = url.hostname.toLowerCase()
+
+  const isOfertaDomain = host === 'oferta.focoemcanto.com'
+  const isRootPath = url.pathname === '/' || url.pathname === ''
+
+  if (isOfertaDomain && isRootPath) {
+    const rewriteUrl = new URL(request.url)
+    rewriteUrl.pathname = '/b/'
+
+    return context.env.ASSETS.fetch(new Request(rewriteUrl.toString(), request))
+  }
+
+  return context.next()
+}
